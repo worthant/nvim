235e75a00023f3a813e1ce60e024d1a52e9ff96a
worthant <b_dvorkin@niuitmo.ru>
2024-08-08 17:11:23 +0400

    :hammer: feat(csv): Add rainbow_csv for aligning csv in neovim

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "mechatroner/rainbow_csv",
+    lazy = false,
+  },
+  {
     "lervag/vimtex",
     lazy = false, -- we don't want to lazy load VimTeX
     -- tag = "v2.15", -- uncomment to pin to a specific release
